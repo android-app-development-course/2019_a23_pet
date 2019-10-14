6ccbd4a8df09a470bd39fb0ce8acec2cd35b4ac6
Dude <oncwnuKYm6bQhil53W1fJIt9yVw4@git.weixin.qq.com>
2019-10-14 15:43:38 +0800

    添加前后端目录结构

---
+++ Server/src/main/java/com/example/gohome/GohomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GohomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GohomeApplication.class, args);
+    }
+
+}

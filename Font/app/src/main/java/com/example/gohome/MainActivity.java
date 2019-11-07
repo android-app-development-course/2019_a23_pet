1c3792a6998ee09232c55cbbe6b9609a96ae09bd
my_ <oncwnuOVPMMoa37WU7RUzCi0DIEQ@git.weixin.qq.com>
2019-11-07 19:27:12 +0800

    color.xml

--- Font/app/src/main/java/com/example/gohome/MainActivity.java
+++ Font/app/src/main/java/com/example/gohome/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.gohome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化控件
+        init();
+    }
+
+
+
+    private void init(){
+        Button btn_user = findViewById(R.id.btn_user);
+        Button btn_organizer = findViewById(R.id.btn_organizer);
+        Button btn_admin = findViewById(R.id.btn_admin);
+
+        btn_organizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,OrganizerHomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

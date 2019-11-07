3ddd8ba32918fc4d015e817515a001b21ae8f75e
Z <1873893450@qq.com>
2019-11-07 23:16:04 +0800

    管理端导航栏

--- Font/app/src/main/java/com/example/gohome/Member/MemberHomeActivity.java
+++ Font/app/src/main/java/com/example/gohome/Member/MemberHomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
@@ -11,60 +12,59 @@ import com.example.gohome.R;
 
 public class MemberHomeActivity extends AppCompatActivity {
 
-    AHBottomNavigation bottomNavigation;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_home);
-        //初始化控件
         init();
+        //初始化控件
     }
 
-    private void init() {
-        bottomNavigation = (AHBottomNavigation) findViewById(R.id.btmNav_mem);
+    private void init(){
+        //绑定控件
+        AHBottomNavigation bottomNavigation = findViewById(R.id.btmNav_mem);
 
-    // Create items
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem("领养", R.drawable.member_adopt, 747474);
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("救助", R.drawable.member_help, 747474);
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("审核", R.drawable.member_check, 747474);
+        //创建items，3个参数分别是item的文字，item的icon，选中item时的整体颜色（该项需要开启）
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("首页", R.drawable.member_adopt, R.color.colorPrimary);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("发布", R.drawable.member_release, R.color.colorPrimary);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("审核", R.drawable.member_check, R.color.colorPrimary);
+        AHBottomNavigationItem item4 = new AHBottomNavigationItem("我的", R.drawable.member_me, R.color.colorPrimary);
 
-    // Add items
+        // Add items
         bottomNavigation.addItem(item1);
         bottomNavigation.addItem(item2);
         bottomNavigation.addItem(item3);
+        bottomNavigation.addItem(item4);
 
-    // Set background color
-        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+        //设置整体背景颜色（如果开启了单个的背景颜色，该项将会无效）
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FFFFFF"));
 
+        //设置item被选中和待选时的颜色
+        bottomNavigation.setAccentColor(Color.parseColor("#98F5FF"));
+        bottomNavigation.setInactiveColor(Color.parseColor("#ffffff"));
 
-    // Change colors
-        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
-        bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
-
-
-
-
-// Use colored navigation with circle reveal effect
+        //是否开启切换item切换颜色
         bottomNavigation.setColored(true);
 
-// Set current item programmatically
+        //设置初始选中的item
         bottomNavigation.setCurrentItem(1);
 
-//// Set listeners
-//        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+        // Set listener
+//        bottomNavigation.setAHBottomNavigationListener(new AHBottomNavigation.AHBottomNavigationListener() {
 //            @Override
-//            public boolean onTabSelected(int position, boolean wasSelected) {
-//                // Do something cool here...
-//                return true;
+//            public void onTabSelected(int position) {
 //            }
 //        });
-//        bottomNavigation.setOnNavigationPositionListener(new AHBottomNavigation.OnNavigationPositionListener() {
-//            @Override public void onPositionChange(int y) {
-//                // Manage the new y position
-//            }
-//        });
-    }
 
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                //点击item时的事件
+//                System.out.println("点击了第："+ position + "个tab");
+                Toast.makeText(MemberHomeActivity.this, "点击了" + position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
 
 }

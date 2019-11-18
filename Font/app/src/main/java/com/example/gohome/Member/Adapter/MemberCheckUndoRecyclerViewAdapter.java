563967d71c8d83416eead912b689fc77ca736711
Wu-peihua <13128684834@163.com>
2019-11-18 15:28:55 +0800

    member端审核界面待审核界面recyclerView设计

---
+++ Font/app/src/main/java/com/example/gohome/Member/Adapter/MemberCheckUndoRecyclerViewAdapter.java
@@ -0,0 +1,67 @@
+package com.example.gohome.Member.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.gohome.R;
+import com.ramotion.foldingcell.FoldingCell;
+
+import java.util.List;
+
+public class MemberCheckUndoRecyclerViewAdapter extends RecyclerView.Adapter<MemberCheckUndoRecyclerViewAdapter.ViewHolder> {
+
+    //数据源
+    private List<String> mList;
+    private Context context;
+
+
+    public MemberCheckUndoRecyclerViewAdapter(Context context, List<String> list){
+        mList = list;
+        this.context = context;
+    }
+
+
+    //负责每一个item
+    public class ViewHolder extends RecyclerView.ViewHolder{
+//        TextView mView;
+        FoldingCell fc_checkUndo;
+        TextView tv_one;
+        TextView tv_two;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+//            mView = itemView.findViewById(R.id.item_tx);
+            fc_checkUndo = itemView.findViewById(R.id.item_fcCheckUndo);
+            tv_one = itemView.findViewById(R.id.tv_one);
+            tv_two = itemView.findViewById(R.id.tv_two);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MemberCheckUndoRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType){
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_member_check_undo_item,null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MemberCheckUndoRecyclerViewAdapter.ViewHolder viewHolder, int position) {
+//        viewHolder.mView.setText(mList.get(position));
+        viewHolder.tv_one.setText(mList.get(position));
+        viewHolder.tv_two.setText(mList.get(position));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList == null ? 0 : mList.size();
+    }
+}
--- Font/app/src/main/java/com/example/gohome/Member/Fragment/MemberCheckUndoFragment.java
+++ Font/app/src/main/java/com/example/gohome/Member/Fragment/MemberCheckUndoFragment.java
@@ -1,16 +1,36 @@
 package com.example.gohome.Member.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.gohome.Member.Adapter.MemberCheckUndoRecyclerViewAdapter;
 import com.example.gohome.R;
+import com.jcodecraeer.xrecyclerview.ProgressStyle;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
 
 public class MemberCheckUndoFragment extends Fragment {
+
+
+    private List<String> infoList;//显示的数据list
+    XRecyclerView xrv_memberCheckUndo;
+    MemberCheckUndoRecyclerViewAdapter memberCheckUndoRecyclerViewAdapter;
+
+    private int refreshTime = 0;
+    private int times = 0;
+    final int itemLimit = 5;
 
 
 
@@ -18,6 +38,113 @@ public class MemberCheckUndoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState ){
         View view = inflater.inflate(R.layout.fragment_member_check_undo,null);
+        ButterKnife.bind(this, view);
+
+        xrv_memberCheckUndo = view.findViewById(R.id.xrv_memberCheckUndo);
+
+
+        initList();
+        init();
+
         return view;
     }
+
+
+    private void init(){
+
+        //管理器
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        //适配器
+        memberCheckUndoRecyclerViewAdapter = new MemberCheckUndoRecyclerViewAdapter(this.getContext(),infoList);
+
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        xrv_memberCheckUndo.setAdapter(memberCheckUndoRecyclerViewAdapter);
+        xrv_memberCheckUndo.setLayoutManager(layoutManager);
+
+        xrv_memberCheckUndo.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
+        xrv_memberCheckUndo.setLoadingMoreProgressStyle(ProgressStyle.BallRotate);
+        xrv_memberCheckUndo.setArrowImageView(R.drawable.iconfont_downgrey);
+
+        xrv_memberCheckUndo
+                .getDefaultRefreshHeaderView()
+                .setRefreshTimeVisible(true);
+
+
+        xrv_memberCheckUndo.setLoadingListener(new XRecyclerView.LoadingListener() {
+            //下拉刷新
+            @Override
+            public void onRefresh() {
+                refreshTime ++;
+                times = 0;
+                new Handler().postDelayed(new Runnable(){
+                    public void run() {
+                        infoList.clear();
+
+                        //模拟加载后的数据
+                        for(int i = 0; i < itemLimit ;i++){
+                            infoList.add("item" + i + "after " + refreshTime + " times of refresh");
+                        }
+                        memberCheckUndoRecyclerViewAdapter.notifyDataSetChanged(); //更新列表
+                        if(xrv_memberCheckUndo != null)
+                            xrv_memberCheckUndo.refreshComplete();
+                    }
+
+                }, 1000);            //refresh data here
+            }
+
+            //上拉加载
+            @Override
+            public void onLoadMore() {
+                Log.e("aaaaa","call onLoadMore");
+                if(times < 2){  //模拟上拉加载三次
+                    new Handler().postDelayed(new Runnable(){
+                        public void run() {
+
+                            //模拟上拉加载的数据
+                            for(int i = 0; i < itemLimit ;i++){
+                                infoList.add("item" + (1 + infoList.size() ) );
+                            }
+
+                            if(xrv_memberCheckUndo != null) {
+                                xrv_memberCheckUndo.loadMoreComplete();
+                                memberCheckUndoRecyclerViewAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }, 1000);
+                } else {  //三次加载完成
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            //模拟上拉加载的数据
+                            for(int i = 0; i < itemLimit ;i++){
+                                infoList.add("item" + (1 + infoList.size() ) );
+                            }
+                            if(xrv_memberCheckUndo != null) {
+                                xrv_memberCheckUndo.setNoMore(true);
+                                memberCheckUndoRecyclerViewAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }, 1000);
+                }
+                times ++;
+            }
+        });
+
+    }
+
+    private void initList() {
+        infoList = new ArrayList<>();
+        infoList.add("keke");
+        infoList.add("lala");
+        infoList.add("alsidfal");
+        infoList.add("keke");
+        infoList.add("lala");
+        infoList.add("alsidfal");
+        infoList.add("keke");
+        infoList.add("lala");
+        infoList.add("alsidfal");
+        infoList.add("keke");
+        infoList.add("lala");
+        infoList.add("alsidfal");
+    }
 }
+
